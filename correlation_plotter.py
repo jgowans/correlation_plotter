718ebe3620475dc5cc158e070b563b1ebde18d56
jgowans <gowans.james@gmail.com>
2014-03-10 09:42:23 +0200

    correlation plotter, angle calculator and logger seem to be working.

--- correlation_plotter.py
+++ correlation_plotter.py
@@ -11,33 +11,22 @@ import scipy.constants
 import logging
 import os
 import json
+import shutil
+import csv
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s:" + logging.BASIC_FORMAT)
 config_file = os.getenv('HOME') + "/correlation_plotter_config"
+results_directory = os.getenv('HOME') + "/correlation_plotter_results/"
 SNAPSHOT_BLOCK_SIZE = 131072
 SAMPLE_FREQUENCY = 800.0e6
-RESAMPLE_FACTOR = 10
+RESAMPLE_FACTOR = 5
 ANTENNA_SPACING_METRES = 5.0
 
-def get_fft_and_axis(signal, resample_factor=1):
-    '''Note: the FFT returned is complex. The axis is in units MHz'''
-    fft = numpy.fft.rfft(signal)
-    fft_axis = numpy.linspace(0, (SAMPLE_FREQUENCY/2.0)/1e6, len(fft), endpoint=False)
-    return fft_axis, fft
-
-def get_upsampled_subsignal_and_axis(signal, start_sample, end_sample):
-    '''Note: end_sample is not included'''
-    subsignal = signal[start_sample:end_sample]
-    upsampled_subsignal = scipy.signal.resample(subsignal, len(subsignal)*RESAMPLE_FACTOR)
-    start_time = (start_sample/SAMPLE_FREQUENCY) * 1e6
-    end_time = (end_sample/SAMPLE_FREQUENCY) * 1e6
-    upsampled_subsignal_axis = numpy.linspace(start_time, end_time, len(upsampled_subsignal), endpoint=False)
-    return upsampled_subsignal_axis, upsampled_subsignal
-
-def cross_correlate(signal0, signal1):
-    return correlation
 
 def plot_initial_signals(signal0, signal1):
+    # remove DC offset
+    signal0 = signal0 - numpy.mean(signal0)
+    signal1 = signal1 - numpy.mean(signal1)
     # get time axis of full signal. Multiply end time by 1e6 to get units of microseconds
     signal_axis = numpy.linspace(0, (SNAPSHOT_BLOCK_SIZE/SAMPLE_FREQUENCY) * 1e6, SNAPSHOT_BLOCK_SIZE, endpoint=False)
 
@@ -61,6 +50,8 @@ def plot_initial_signals(signal0, signal1):
     ax_signal1.plot(signal_axis, signal1)
     ax_fft_signal1.plot(fft_axis, numpy.abs(fft_signal1))
     ax_fft_signal1_phase.plot(fft_axis, numpy.angle(fft_signal1))
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
     fig.show()
 
 def get_triggered_snapshot():
@@ -73,13 +64,16 @@ def get_triggered_snapshot():
     adc0_data = fpga.snapshot_get("adc0_snap", wait_period=-1, arm=False)["data"]
     adc1_data = fpga.snapshot_get("adc1_snap", wait_period=-1, arm=False)["data"]
     fpga.write_int("trig_gate", 0)
+    with open("/tmp/sig0", "wb") as f:
+        f.write(adc0_data)
+    with open("/tmp/sig1", "wb") as f:
+        f.write(adc1_data)
+
 
     logging.info("captured. unpacking data")
     signal0 = struct.unpack(str(SNAPSHOT_BLOCK_SIZE ) + "b", (adc0_data))
     signal1 = struct.unpack(str(SNAPSHOT_BLOCK_SIZE ) + "b", (adc1_data))
     # remove ADC offset
-    signal0 = signal0 -  numpy.mean(signal0)
-    signal1 = signal1 - numpy.mean(signal1)
     plot_initial_signals(signal0, signal1)
     #now plot the signals
     return signal0, signal1
@@ -94,6 +88,9 @@ def plot_correlation_and_get_angle(signal0, signal1):
     
     sub_signal0 = signal0[start_sample:end_sample]
     sub_signal1 = signal1[start_sample:end_sample]
+    # remove DC offset
+    sub_signal0 = sub_signal0 - numpy.mean(sub_signal0)
+    sub_signal1 = sub_signal1 - numpy.mean(sub_signal1)
     upsampled_sub_signal0 = scipy.signal.resample(sub_signal0, len(sub_signal0)*RESAMPLE_FACTOR)
     upsampled_sub_signal1 = scipy.signal.resample(sub_signal1, len(sub_signal1)*RESAMPLE_FACTOR)
     start_time = (start_sample/SAMPLE_FREQUENCY) * 1e6
@@ -101,8 +98,8 @@ def plot_correlation_and_get_angle(signal0, signal1):
     upsampled_sub_signal_axis = numpy.linspace(start_time, end_time, len(upsampled_sub_signal0), endpoint=False)
 
     # get FFT of  subsignal
-    fft_sub_signal0 = numpy.fft.rfft(signal0)
-    fft_sub_signal1 = numpy.fft.rfft(signal1)
+    fft_sub_signal0 = numpy.fft.rfft(sub_signal0)
+    fft_sub_signal1 = numpy.fft.rfft(sub_signal1)
     fft_sub_signal_axis = numpy.linspace(0, (SAMPLE_FREQUENCY/2.0)/1e6, len(fft_sub_signal0), endpoint=False)
     # get the correlation of the upsampled signals
     correlation = numpy.correlate(upsampled_sub_signal0, upsampled_sub_signal1, "full")
@@ -122,7 +119,10 @@ def plot_correlation_and_get_angle(signal0, signal1):
     ax_signal1.plot(upsampled_sub_signal_axis, upsampled_sub_signal1)
     ax_fft_signal1.plot(fft_sub_signal_axis, numpy.abs(fft_sub_signal1))
     ax_correlation.plot(correlation)
+    mng = plt.get_current_fig_manager()
+    mng.resize(*mng.window.maxsize())
     fig.show()
+    plt.savefig("/tmp/pyplot_dump.png")
 
     index_of_max_value = correlation.argmax()
     samples_delay = len(upsampled_sub_signal0) - index_of_max_value - 1
@@ -130,9 +130,25 @@ def plot_correlation_and_get_angle(signal0, signal1):
     time_delay = (1.0/(SAMPLE_FREQUENCY * RESAMPLE_FACTOR)) * samples_delay
     delay_max = ANTENNA_SPACING_METRES / scipy.constants.c
     print "delay max: " + str(delay_max) + "  time_delay: " + str(time_delay)
-    angle = numpy.arcsin(time_delay / delay_max)
-    print "angle of arrival: " + str(round(angle, 3)) + " radians or " + str(round(numpy.degrees(angle), 3)) + " degrees"
+    angle = round(numpy.degrees(numpy.arccos(time_delay / delay_max)), 2)
+    print "angle of arrival: " + str(angle) + " degrees"
     return angle
+
+def log_to_file(signal0, signal1, angle):
+    shutil.copyfile("/tmp/sig0",  results_directory + "/adc_dumps/" + timestamp + "-signal0")
+    shutil.copyfile("/tmp/sig1",  results_directory + "/adc_dumps/" + timestamp + "-signal1")
+    shutil.copyfile("/tmp/pyplot_dump.png", results_directory + "/pyplot_dumps/" + timestamp + ".png")
+    # start building up summary string. format:
+    # time, lat, long, antenna_angle, signal_angle_relative_to_antenna
+    summary = []
+    summary.append(timestamp)
+    summary.append(global_config["latitude"])
+    summary.append(global_config["longitude"])
+    summary.append(global_config["antenna_angle_degrees"])
+    summary.append(angle) #angle of arival 
+    with open(results_directory + "summary.csv", "a") as summary_file:
+        csv_writer = csv.writer(summary_file)
+        csv_writer.writerow(summary)
 
 def verify_paramaters():
     print "{:f} N   {:f} W".format(global_config["latitude"],  global_config["longitude"])
@@ -160,26 +176,16 @@ raw_input("Ready to continue when you are... ")
 
 while True:
     signal0, signal1 = get_triggered_snapshot()
+    timestamp = time.strftime("%y-%m-%d-%H-%M-%S")
     user_response = raw_input("Use this signal [y], ignore it [n], or adjust the trigger level [a]:  ")
     if user_response == "a":
+        print "Trigger is currently {:d}".format(fpga.read_int("trig_level"))
         new_trigger = int(raw_input("What value should the trigger be?:  "))
         fpga.write_int("trig_level", new_trigger)
     if  user_response == "y":
         angle = plot_correlation_and_get_angle(signal0, signal1)
-        user_reponse = raw_input("Should this vecor be logged to file? [y/n]  ")
+        user_response = raw_input("Should this vecor be logged to file? [y/n]  ")
         if user_response == "y":
-            log_to_file(angle) #this should also log the PNG!
+            log_to_file(signal0, signal1, angle) #this should also log the PNG!
+    plt.close("all")
 
-
-
-# all of the above stuff should be put into methods. The algorithm should be as follows:
-# you are at <coordinates>. confirm?
-# run un-triggered dump.
-# provide oportunity to set trigger level
-# wait for trigger
-# plot fft
-# interested?
-# correlation interval?
-# plot correlation and output AOA.
-# save and log?
-# write coordinates and AOA to log file. save PNG to directory (name = time)
