73eebe3e1a088a2921ce1dfa56f832545e2ee6da
James Gowans <gowans.james@gmail.com>
2014-05-02 11:17:14 +0200

    longer capture and pulse end detection now working

--- rfi_capture.py
+++ rfi_capture.py
@@ -15,20 +15,12 @@ import shutil
 import csv
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s:" + logging.BASIC_FORMAT)
-config_file = os.getenv('HOME') + "/correlation_plotter_config"
-results_directory = os.getenv('HOME') + "/correlation_plotter_results/"
+results_directory = os.getenv('HOME') + "/rfi_capture_results/"
 SNAPSHOT_BLOCK_SIZE =4*4*(2**22) 
 SAMPLE_FREQUENCY = 800.0e6
-RESAMPLE_FACTOR = 5.0
-#ANTENNA_SPACING_METRES = 4.32 #this is the beam length
-ANTENNA_SPACING_METRES = 3.53
 
 
 def plot_initial_signal(signal0):
-    # remove DC offset
-    #signal0 = signal0 - np.mean(signal0)
-    #signal1 = signal1 - np.mean(signal1)
-    # get time axis of full signal. Multiply end time by 1e6 to get units of microseconds
     signal_axis = np.linspace(0, (SNAPSHOT_BLOCK_SIZE/SAMPLE_FREQUENCY) * 1e6, SNAPSHOT_BLOCK_SIZE, endpoint=False)
 
     # get the FFT of the signal
@@ -42,7 +34,8 @@ def plot_initial_signal(signal0):
     #ax_fft_signal0_phase = fig.add_subplot(224, sharex=ax_fft_signal0)
     # TODO: lable the axes
     # add the plots
-    ax_signal0.plot(signal_axis, signal0, "r,")
+    ax_signal0.plot(signal0, "r,")
+    #ax_signal0.plot(signal_axis, signal0, "r,")
     #ax_signal1.plot(signal_axis, signal1, ".-")
     #ax_fft_signal0.plot(fft_axis, numpy.abs(fft_signal0))
     #ax_fft_signal0_phase.plot(fft_axis, numpy.angle(fft_signal0))
@@ -53,18 +46,19 @@ def plot_initial_signal(signal0):
 def get_triggered_snapshot():
     # arm the snapshot blocks
     fpga.snapshot_arm("dram_snap", man_trig=False, man_valid=False, offset=-1, circular_capture=False)
-    fpga.snapshot_arm("bram_snap", man_trig=False, man_valid=False, offset=-1, circular_capture=False)
-    # enable the triger gate
-    fpga.write_int("trig_gate", 1)
+    fpga.write_int("trig_arm", 0)
+    fpga.write_int("trig_arm", 1)
     logging.info("trigger gate enabeled. waiting for capture event")
     #TODO: Logic to actually detect trigger event!
-    #bram_data = fpga.snapshot_get("bram_snap", wait_period=-1, arm=False)["data"]
-    #logging.info("bram read")
+    while fpga.read_int("triggered") == 0:
+        pass
+    fpga.write_int("trig_arm", 0)
+    logging.info("got trigger. now dumping dram")
+    length_to_read = 4*((2*4096) + fpga.read_int("pulse_length")) #padding of 4096 "clock cycles" each side, where each clock cycle is 4 samples
+    length_to_read = min(length_to_read, SNAPSHOT_BLOCK_SIZE) # ensure we don't try to read more than the maximum number of camptured samples
     #2^11 DRAM entries, each containing 4 ADC snapshot, each snapshot containing 4 samples
-    dram_data = fpga.read_dram(SNAPSHOT_BLOCK_SIZE)
+    dram_data = fpga.read_dram(length_to_read)
     logging.info("dram read")
-    fpga.write_int("trig_gate", 0)
-
     logging.info("captured. unpacking data")
     #signal0 = struct.unpack(str(SNAPSHOT_BLOCK_SIZE ) + "b", (dram_data))
     signal0 = np.frombuffer(dram_data, dtype=np.int8)
@@ -95,16 +89,19 @@ def log_to_file(signal0):
         csv_writer.writerow(summary)
 
 #ENTRY POINT
-global_config = json.load(open(config_file))
-
 # connect to FPGA and program
 logging.info("starting programming and configuring")
 fpga = corr.katcp_wrapper.FpgaClient("localhost", 7147, timeout=40)
-time.sleep(1)
 #fpga.progdev("jgowans_snapshot_no_fft_2014_Apr_17_1307.bof")
 time.sleep(1)
-fpga.write_int("trig_level", 1)
-get_triggered_snapshot()
+fpga.write_int("trig_level", 10)
+signal0 = get_triggered_snapshot()
+while True:
+    fpga.write_int("trig_arm", 0)
+    fpga.write_int("trig_arm", 1) # assert a rising edge
+    while fpga.read_int("triggered") == 0:
+        pass
+    logging.info("got one! of length " + str(fpga.read_int("pulse_length")))
 new_trigger = int(raw_input("What value should the trigger be?:  "))
 fpga.write_int("trig_level", new_trigger)
 time.sleep(1)
