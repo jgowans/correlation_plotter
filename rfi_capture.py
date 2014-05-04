71ddc7f7564ee94268247eaa935528fda67d85e9
James Gowans <gowans.james@gmail.com>
2014-05-04 19:33:06 +0200

    something vaguely resembling a working system

--- rfi_capture.py
+++ rfi_capture.py
@@ -7,20 +7,20 @@ import corr
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s:" + logging.BASIC_FORMAT)
 results_directory = os.getenv('HOME') + "/rfi_capture_results/"
-SNAPSHOT_BLOCK_SIZE =4*4*(2**22) 
-SAMPLE_FREQUENCY = 3600.0e6 # microseconds and megahertz
+SNAPSHOT_BLOCK_SIZE =4*4*(2**23)/128 
+SAMPLE_FREQUENCY = 3600.0e6 
 
 
-def plot_initial_signal(signal0):
-    signal_axis = np.linspace(0, (len(signal0)/SAMPLE_FREQUENCY) * 1e6, len(signal0), endpoint=False)
+def plot_signal(signal):
+    signal_axis = np.linspace(0, (len(signal)/SAMPLE_FREQUENCY) * 1e6, len(signal), endpoint=False)
     fig = plt.figure()
     ax_signal0 = fig.add_subplot(111)
-    ax_signal0.plot(signal_axis, signal0, ",")
+    ax_signal0.plot(signal_axis, signal, ",")
     mng = plt.get_current_fig_manager()
     mng.resize(*mng.window.maxsize())
     plt.show()
 
-def get_triggered_snapshot():
+def get_triggered_snapshot(read_full_dram=False):
     # arm the snapshot blocks
     fpga.snapshot_arm("dram_snap", man_trig=False, man_valid=False, offset=-1, circular_capture=False)
     fpga.write_int("trig_arm", 0)
@@ -33,58 +33,48 @@ def get_triggered_snapshot():
     logging.info("got trigger. now dumping dram")
     # length = padding of 2^15 "clock cycles" each side of the pulse, where each clock cycle is 16 samples.
     # the padding takes about 1 second to read out. Worth it, I think!! :-)
-    length_to_read = 16*((2*(2**15)) + fpga.read_int("pulse_length")) 
+    length_to_read = 16*((2*(2**14)) + fpga.read_int("pulse_length")) 
     # ensure we don't try to read more than the maximum number of camptured samples
-    length_to_read = min(length_to_read, SNAPSHOT_BLOCK_SIZE) 
-    length_to_read = SNAPSHOT_BLOCK_SIZE
+    length_to_read = min(length_to_read, SNAPSHOT_BLOCK_SIZE)
+    if read_full_dram == True:
+        length_to_read = SNAPSHOT_BLOCK_SIZE
     dram_data = fpga.read_dram(length_to_read)
     logging.info("dram read. unpacking data")
-    signal0 = np.frombuffer(dram_data, dtype=np.int8)
-    plot_initial_signal(signal0)
-    return signal0
+    signal = np.frombuffer(dram_data, dtype=np.int8)
+    logging.info("unpack complete")
+    return signal
 
-def log_to_file(signal0, timestamp):
+def log_to_file(signal):
     if raw_input("log signal to disk? [y/n]:  ") == "n":
         return
-    #annotation = raw_input("annotate this signal please: ")
-    #np.save("/tmp/rfi_signal", {"annotation": annotation, "data": signal0})
-    np.save("/tmp/rfi_signal", signal0)
+    timestamp = time.strftime("%y-%m-%d-%H-%M-%S")
+    np.save("/tmp/rfi_signal", signal)
     shutil.copyfile("/tmp/rfi_signal.npy",  results_directory + timestamp + ".npy")
     logging.info("signal in both /tmp and results directory")
 
 #ENTRY POINT
 # connect to FPGA and program
-logging.info("starting programming and configuring")
-fpga = corr.katcp_wrapper.FpgaClient("localhost", 7147, timeout=120)
-#fpga.progdev("jgowans_snapshot_no_fft_2014_Apr_17_1307.bof")
-time.sleep(1)
-fpga.write_int("trig_level", 10)
-signal0 = get_triggered_snapshot()
-timestamp = time.strftime("%y-%m-%d-%H-%M-%S")
-log_to_file(signal0, timestamp)
+logging.info("connecting to FPGA")
+fpga = corr.katcp_wrapper.FpgaClient("localhost", 7147, timeout=80)
+# initial read of current data
+fpga.write_int("trig_level", 0)
+if raw_input("look at untriggered data? [y/n]  ") == "y":
+    signal = get_triggered_snapshot(read_full_dram=True)
+    print "largest value in the signal = " + str(max( abs(max(signal)), abs(min(signal))))
+    plot_signal(signal)
+    log_to_file(signal)
+fpga.write_int("trig_level", 3)
 while True:
-    fpga.write_int("trig_arm", 0)
-    fpga.write_int("trig_arm", 1) # assert a rising edge
-    while fpga.read_int("triggered") == 0:
-        pass
+    signal = get_triggered_snapshot()
     logging.info("got one! of length " + str(fpga.read_int("pulse_length")))
-new_trigger = int(raw_input("What value should the trigger be?:  "))
-fpga.write_int("trig_level", new_trigger)
-time.sleep(1)
-raw_input("Ready to continue when you are... ")
-
-while True:
-    signal0 = get_triggered_snapshot()
-    print("and the amplitude was: {:d}".format(int(max(signal0))))
-    timestamp = time.strftime("%y-%m-%d-%H-%M-%S")
-    user_response = raw_input("Use this signal [y], ignore it [n], or adjust the trigger level [a]:  ")
-    #user_response =  'n'
-    if user_response == "a":
-        print "Trigger is currently {:d}".format(fpga.read_int("trig_level"))
-        new_trigger = int(raw_input("What value should the trigger be?:  "))
-        fpga.write_int("trig_level", new_trigger)
-    if  user_response == "y":
-        user_response = raw_input("Should this vecor be logged to file? [y/n]  ")
-        if user_response == "y":
-            log_to_file(signal0) #this should also log the PNG!
     plt.close('all')
+    if raw_input("look at signal? [y/n]  ") == "y":
+        plot_signal(signal)
+    log_to_file(signal)
+    print "Trigger is currently {:d}".format(fpga.read_int("trig_level"))
+    new_trigger = raw_input("What value should the trigger be?:  ")
+    try:
+        fpga.write_int("trig_level", int(new_trigger))
+    except:
+        pass
+
--- rfi_looker.py
+++ rfi_looker.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import scipy.signal
 
 results_directory = os.getenv('HOME') + "/rfi_capture_results/"
+SAMPLE_FREQUENCY = 3600.0 # MHz and us
+ADC_SCALE_VALUE = 707.94
 
 # algorithm:
 # open a .npy file (or do the disk buffer thing)
@@ -15,17 +17,34 @@ if filename == "": # default to the most recent file
     filename = "/tmp/rfi_signal.npy"
 else:
     filename = results_directory + filename
-
-# get the annotation here
-data = np.load(filename)
-axis = np.linspace(0, len(data), len(data), endpoint=False)
-t0 = time.time()
-data_decimated = scipy.signal.decimate(data, 30)
-print time.time() - t0
-axis_decimated = np.linspace(0, len(data), len(data_decimated), endpoint=False)
-#plt.plot(axis, data, "b.-")
-plt.plot(axis_decimated, data_decimated, "r.-")
+signal = np.load(filename)
+decimation_factor = int(len(signal)/2**10) + 1
+print "decimation factor: " + str(decimation_factor)
+if decimation_factor >= 2 :
+    signal_decimated = scipy.signal.decimate(signal, decimation_factor, n=1, ftype="fir")
+else:
+    signal_decimated = signal
+print "len : " + str(len(signal_decimated))
+axis = np.linspace(0, decimation_factor * len(signal_decimated)/SAMPLE_FREQUENCY, len(signal_decimated), endpoint=False)
+plt.plot(axis, signal_decimated, "b-")
 plt.show()
 # plot the signal decimated by a paramamter (defualt: 1)
+
 # ask the user to input a subplot time
+start_time = float(raw_input("At what time (microseconds) does the signal start?  "))
+end_time = float(raw_input("At what time (microseconds) does the signal end?   "))
+start_sample = int( start_time * SAMPLE_FREQUENCY )
+end_sample = int( end_time * SAMPLE_FREQUENCY )
+
+subsignal = signal[start_sample:end_sample]
+subsignal_axis = np.linspace(start_time, end_time, len(subsignal), endpoint=False)
+spectrum = np.fft.rfft(subsignal)
+spectrum_axis = np.linspace(0, SAMPLE_FREQUENCY/2, len(spectrum), endpoint=False)
+
+plt.subplot(211)
+plt.plot(subsignal_axis, subsignal)
+plt.subplot(212)
+plt.plot(spectrum_axis, 10*np.log10( np.abs(spectrum) / (ADC_SCALE_VALUE*len(spectrum) )))
+plt.show()
+
 # plot the subplot and the fft of the subplot
