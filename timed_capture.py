cb5e70cb7a3f0bf54e8eeb449294af02d38505e6
James Gowans <gowans.james@gmail.com>
2015-03-14 12:49:10 +0200

    Timed capture brought in sync with model

--- timed_capture.py
+++ timed_capture.py
@@ -11,10 +11,10 @@ parser = argparse.ArgumentParser(description = 'Generate output clock and captur
 parser.add_argument('--calibrate', action='store_true')
 parser.add_argument('--plot_time', action='store_true')
 parser.add_argument('--plot_freq', action='store_true')
-parser.add_argument('--force_gpio_trigger', action='store_true')
 parser.add_argument('--force_snap_trigger', action='store_true')
-parser.add_argument('--pos_time_us', default=0, type=float)
-parser.add_argument('--neg_time_us', default=0, type=float)
+parser.add_argument('--pos_delay_ms', default=0, type=float)
+parser.add_argument('--neg_delay_ms', default=0, type=float)
+parser.add_argument('--period_ms', default=0, type=float)
 parser.add_argument('--host', default='192.168.14.30', type=str)
 parser.add_argument('--samples', default=2**20, type=int)
 parser.add_argument('--capture_dir', default='/tmp/', type=str)
@@ -29,34 +29,42 @@ time.sleep(0.1)
 clock_speed = int(fpga.est_brd_clk())
 print('got clk speed of: {cs}'.format(cs = clock_speed))
 
-pos_edge_samples = int((args.pos_time_us / 10**6)/(1.0/clock_speed))
-print('for pos edge time of {t} us, setting {s} samples'.format(t = args.pos_time_us, s = pos_edge_samples))
-fpga.write_int('pos_edge_time', pos_edge_samples)
+pos_delay_samples = int((args.pos_delay_ms / 10**3)/(1.0/clock_speed))
+print('for positive edge delay time of {t} ms, setting {s} samples'.format(t = args.pos_delay_ms, s = pos_delay_samples))
+fpga.write_int('pos_delay', pos_delay_samples)
 
-neg_edge_samples = int((args.neg_time_us / 10**6)/(1.0/clock_speed))
-print('for neg edge time of {t} us, setting {s} samples'.format(t = args.neg_time_us, s = neg_edge_samples))
-fpga.write_int('neg_edge_time', neg_edge_samples)
-
-fpga.write_int('trig_level', args.trig_level)
-fpga.write_int('force_trigger_arm', args.force_gpio_trigger)
+neg_delay_samples = int((args.neg_delay_ms / 10**3)/(1.0/clock_speed))
+print('for negative edge delay time of {t} ms, setting {s} samples'.format(t = args.neg_delay_ms, s = neg_delay_samples))
+fpga.write_int('neg_delay', neg_delay_samples)
 
 while True:
     fpga.snapshot_arm('snapshot', man_trig=args.force_snap_trigger, man_valid=False, offset=-1, circular_capture=False)
-    edge = 'pos' if fpga.read_uint('gpioa_state_snap') else 'neg'
+    while (fpga.read_uint('triggered_latch') == 0) and (args.force_snap_trigger == False):
+        pass
+    fpga.write_int('reset_triggered_latch', 1)
+    fpga.write_int('led_1_control', 0)
+    edge = 'pos' if fpga.read_uint('gpioa_0_status') else 'neg'
     snap_time = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S_%f')
     print('got edge: {e} at time: {t}'.format(e = edge, t = snap_time))
-    time.sleep(0.1)
+    time.sleep(0.01)
     raw = fpga.read_dram(args.samples)
     signal = np.frombuffer(raw, dtype=np.int8)
     file_name = '{d}/{t}_{e}'.format(d = args.capture_dir, t = snap_time, e = edge)
     with open(file_name, 'w') as f:
         f.write(signal)
     if args.plot_time == True:
+        delay = args.pos_delay_ms if edge == 'pos' else args.neg_delay_ms
+        edge_full = 'POSITIVE' if edge == 'pos' else 'NEGATIVE'
+        title = 'Time domain of {e} edge with a delay of {t} ms'.format(e = edge_full, t = delay)
+        time_axis = np.linspace(delay, delay + len(signal)/1600000, len(signal)) # work in milliseconds
         plt.plot(signal)
+        plt.title(title)
         plt.show()
     if args.plot_freq == True:
         signal_fft = np.fft.rfft(signal)
         fft_axis = np.linspace(0, 1600/2, len(signal_fft), endpoint=False)
         plt.plot(fft_axis, np.abs(signal_fft))
         plt.show()
+    fpga.write_int('reset_triggered_latch', 0)
+    fpga.write_int('led_1_control', 1)
 
