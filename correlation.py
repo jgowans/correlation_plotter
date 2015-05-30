c78648c7d0cbe9d2ee952bd772dd52dc454685bd
James Gowans <gowans.james@gmail.com>
2015-05-30 18:28:47 +0200

    Correlator using interfaces not direct access
    - Control register abstrated into class and Correlator goes through this
    when adjusting register
    - Snapshot class written which abstracts arming, getting and unpacking
    data from snap.
    - Correlation object to use this Snapshot class to get its data.
    - Correlator not accessing snaps directly, but rather going through
    Correlations which manage getting their own data.

--- correlation.py
+++ correlation.py
@@ -5,13 +5,25 @@
 '''
 
 import numpy as np
+from snapshot import Snapshot
 
 class Correlation:
-    def __init__(self, name, signal):
+    def __init__(self, combination, fpga):
         '''
-        name: what antennas is this the correlation of? Eg: (0, 0)
-        raw_signal: An array of complex numbers
+        combination -- what antennas is this the correlation of? Eg: (0, 0)
         '''
-        self.name = name
-        self.signal = signal
+        self.combination = combination
+        self.fpga = fpga
+        snap_name = "snap_{a}x{b}".format(a = combination[0], b = combination[1])
+        # big endian 4 byte signed ints in snapblock which should be converted to complex numbers
+        self.snapshot = Snapshot(fpga, snap_name, dtype='>i4', cvalue=True)
+        # TODO: adjust autos to be power (real valued only)
 
+    def signal(self):
+        return self.snapshot.signal
+
+    def fetch_signal(self, force=False):
+        self.snapshot.fetch_signal(force)
+
+    def arm(self):
+        self.snapshot.arm()
--- correlator.py
+++ correlator.py
@@ -6,6 +6,8 @@ Interface to the ROACH.
 import logging
 import corr
 from correlation import Correlation
+from snapshot import Snapshot
+from control_register import ControlRegister
 import itertools
 import numpy as np
 import time
@@ -21,6 +23,7 @@ class Correlator:
         fs -- sample frequency of antennas. (default 800e6; 800 MHz)
         logger -- logger to use. (default: new default logger)
         """
+        self.logger = logger
         self.fpga = corr.katcp_wrapper.FpgaClient(ip_addr)
         time.sleep(0.1)
         self.num_channels = num_channels
@@ -28,74 +31,41 @@ class Correlator:
         # only 0x0 has been implemented
         #self.auto_combinations = [(x, x) for x in range(num_channels)] # [(0, 0), (1, 1), (2, 2), (3, 3)]
         self.auto_combinations = [(0, 0)]
-        self.logger = logger
-        self.logger.info("My name is: {n}".format(n = __name__))
+        self.correlations = {}
+        for comb in (self.cross_combinations + self.auto_combinations):
+            self.correlations[comb] = Correlation(comb, self.fpga)
+            self.correlations[comb].fetch_signal(force=True)  # ensure populated with some data
+        self.control_register = ControlRegister(self.fpga, self.logger.getChild('control_reg'))
 
-    def get_crosses(self):
-        """Reads the snapshot blocks for all cross correlations and populates Correlation objects
+    def fetch_crosses(self):
+        """ Updates the snapshot blocks for all cross correlations
         """
-        self.crosses = self.get_combinations(self.cross_combinations)
-        return self.crosses
+        self.crosses = self.fetch_combinations(self.cross_combinations)
 
-    def get_autos(self):
-        """Reads the snapshot blocks for all auto correlations and populates Correlation objects"""
-        self.autos = self.get_combinations(self.auto_combinations)
-        return self.autos
+    def fetch_autos(self):
+        """ Reads the snapshot blocks for all auto correlations and populates Correlation objects"""
+        self.fetch_combinations(self.auto_combinations)
 
-    def get_all(self):
-        """Popules both cross correlations and auto correlations.
+    def fetch_all(self):
+        """ Popules both cross correlations and auto correlations.
         Returns Correlation objects for crosses and autos
         """
-        all_correlations = self.get_combinations(self.cross_combinations + self.auto_combinations)
-        self.crosses = all_correlations[0:len(self.cross_combinations)]
-        self.autos = all_correlations[-len(self.auto_combinations):]
-        return all_correlations
+        self.fetch_combinations(self.cross_combinations + self.auto_combinations)
 
-    def get_combinations(self, combinations):
+    def fetch_combinations(self, combinations):
         """ Takes an array of X correlations and returns the Correlation objects
         """
-        correlations = []
-        self.block_trigger()
+        self.control_reg.block_trigger()
         for comb in combinations:
             self.arm_combination(comb)
-        self.allow_trigger()
+        self.control_reg.allow_trigger()
         for comb in combinations:
-            correlations.append(Correlation(comb, self.get_vector_from_snap(comb)))
-        return correlations
-
-    def get_vector_from_snap(self, combination):
-        """ Returns an array of complex numbers for the snap block associated with a particular combination
-        """
-        raw_data = self.fpga.snapshot_get(self.get_snap_name(combination), arm=False, wait_period=12)['data']
-        raw_components = np.frombuffer(raw_data, dtype=np.dtype('>i4')) # big endian 4 byte ints
-        # the imaginary component appears first.
-        #TODO: VERIFY THIS!!
-        #signal = [complex(raw_components[i+1], raw_components[i]) for i in range(0, len(raw_components), 2)]
-        signal = [complex(raw_components[i], raw_components[i+1]) for i in range(0, len(raw_components), 2)]
-        self.logger.debug("From snap: {c} a signal of length {l} was read".format(c = combination, l = len(signal)))
-        return signal
+            self.correlations[comb].fetch_signal()
 
     def arm_combination(self, combination):
-        self.fpga.snapshot_arm(self.get_snap_name(combination))
-        self.logger.debug("Armed snapshot combination: {c}".format(c = combination))
-
-    def block_trigger(self):
-        ctrl = self.fpga.read_uint('control')
-        ctrl &= ~(1 << 1) # clear bit 1
-        self.fpga.write_int('control', ctrl)
-        self.logger.debug("Snap trigger blocked")
-
-    def allow_trigger(self):
-        ctrl = self.fpga.read_uint('control')
-        ctrl |= (1 << 1) # set bit 1
-        self.fpga.write_int('control', ctrl)
-        self.logger.debug("Snap trigger allowed")
-
-    def get_snap_name(self, combination):
-        """Returns the name of the snapshot block for a particular cross combination (tuple)
-        Eg: (1, 3) -> snap_1x3
+        """ Arms the snapshot block associated with the correlation combination
         """
-        return "snap_{a}x{b}".format(a = combination[0], b = combination[1])
+        self.correlations[combination].arm()
 
     def set_accumulation_len(self, acc_len):
         """The number of vectors which should be accumulated before being snapped. 
@@ -107,22 +77,11 @@ class Correlator:
     def set_shift_schedule(self, shift_schedule):
         """ Defines the FFT bit shift schedule
         """
-        ctrl = self.fpga.read_uint('control')
-        ctrl &= ~(0xFFF << 2)  # clear bit 2 -> bit 14
-        shift_schedule &= (0xFFF) # ensure only 12 bits
-        ctrl |= (shift_schedule << 2)
-        self.fpga.write_int('control', ctrl)
-        self.logger.info("FFT shift schedule set to {s}".format(s = shift_schedule))
-        self.re_sync()
+        self.control_reg.set_shift_schedule(shift_schedule)
 
     def re_sync(self):
-        ctrl  = self.fpga.read_uint('control')
-        ctrl &= 0xFFFE  # clear bit 0
-        self.fpga.write_int('control', ctrl)
-        ctrl |= 0x1  # set bit 0
-        self.fpga.write_int('control', ctrl)
-        ctrl &= 0xFFFE  # clear bit 0
-        self.fpga.write_int('control', ctrl)
-        self.logger.info("Re-issed a sync pulse")
-        time.sleep(0.1)
+        self.control_reg.pulse_sync()
+
+    def reset_accumulation_counter(self):
+        self.control_reg.reset_accumulation_counter()
 
